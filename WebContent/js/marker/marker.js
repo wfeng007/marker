8244ec325a2e8b5ebdf2be3ba67790dde472b102
wfeng007 <wfeng007@163.com>
2014-02-03 20:59:08 +0800

    v0.2 修改了标注以比例稳准而不是具体位置数字 增加可以使用已有img dom作为底图源

--- WebContent/js/marker/marker.js
+++ WebContent/js/marker/marker.js
@@ -1,8 +1,11 @@
 /**
- *
- * Author: tianchungang
+ * 
+ * 图片标记器
+ * Author: tianchungang,wangfeng
+ * e-mail: wfeng007@163.com
  * Date: 14-1-12
  * Time: 下午8:22
+ * version: 0.2
  */
 (function ($)
 {
@@ -24,23 +27,28 @@
     $.fn.marker = function (options)
     {
         this.options = {
-            data:null,                                    //本地渲染数据（坐标与标记图标的地址）
-            url:"",                                       //远程数据url //这个其实是编辑模式中，当用户点击标注时，在对话框中显示内容的url。frame为true时，为frame引用的路径。frame为false时是用post获取该路径的数据。
-            viwUrl:"",                                     //远程展示数据url
+        	//基本参数
             picUrl:"",                                     //底层图片的地址
+            markId:"",                                     //指定用来标注模板对象集合的ID //如:ul li组合。ul id="markerTag"，li包含标注图片。
             markerUrl:"",                                  //标注点 数据远程url  //获取标注信息（如x、y，数据内容等）的地址，使用post获取。该部分不为null才initjq对象时才会调用内部的loadData函数。
             picWidth:null,                                 //图片展示宽度 //createHtml函数中使用
-            picHeight:null,                                 //图片展示高度 //createHtml函数中使用
+            picHeight:null,                                //图片展示高度 //createHtml函数中使用
             isEdit:false,                                  //设置为true，双击标注图标能进行属性编辑 //确定标记板当前为何种模式
+            //edit mode 编辑模式扩展参数
             popWidth:200,                                  //弹出窗口宽度 //编辑模式时点击标注弹出框的大小
             popHeight:200,                                 //弹出窗口内容高度  //编辑模式时点击标注弹出框的大小
+            frame:false,								   //编辑模式时点击标注弹出框是否是iframe方式
+            url:"",                                       //远程数据url //这个其实是编辑模式中，当用户点击标注时，在对话框中显示内容的url。frame为true时为frame引用的路径。frame为false时是用post获取该路径的数据。            
             popTitle:"标注坐标点",                          //弹窗标题  //编辑模式时点击标注弹出框的大小
             hasSelfCur:false,                              //是否采用自定义cursor //点击标记对象模板后自定义的标记样式。是标记对象模板的src属性制定。
             okCallBack:null,                               //弹窗确定按钮的回调函数，可添加保存数据功能  //编辑模式时点击标注弹出框提供了默认的ok按钮。按钮点击后的回调。(编辑模式下的frame为true时。)
-            markId:"",                                     //指定用来标注的图标ID //如:ul li组合。
-            frame:false,								   //编辑模式时点击标注弹出框是否是frame方式
-            viewClick:null,								   //
-            cancelCallBack:null								//编辑模式时点击标注弹出框提供了默认的cancel按钮。按钮点击后的回调。(编辑模式下的frame为true时。)
+            cancelCallBack:null,						//编辑模式时点击标注弹出框提供了默认的cancel按钮。按钮点击后的回调。(编辑模式下的frame为true时。)
+            //view mode 展示模式扩展参数
+            viewClick:null,								   //在展示编辑模式下为标注提供的单击扩展回调，如果为null则直接为alert显示标注数据。
+            
+            //未使用的参数或遗留参数
+            data:null,                                    //本地渲染数据（坐标与标记图标的地址） //TODO 似乎暂时没有用到
+            viwUrl:""                                     //远程展示数据url  // TODO 似乎暂时没有用到。
         };
         $.extend(this.options,options); //参数合并到this.options参数对象
         return $.marker.init(this); //返回初始化的marker对象。
@@ -58,7 +66,7 @@
             var p = this.options = obj.options;
             var g = this; //marker对象
             g.setObj(obj); //jq对象
-            g.setFlag(-1); //？
+            g.setFlag(-1); //设置标注模板对象选中状态，当状态大于0时,可以单击标注板
             g.createHtml(); //render界面
             g.initCursor(); //
             g.initEvent(); //增加交互用的事件
@@ -72,17 +80,38 @@
         },
         //创建界面的html元素
         //包括一个外部相对布局的外框，内部的底层图片、位于图片上层的标注所在的蒙版（透明并覆盖提成图片。markerlayer）。
-        //图片显示大小由插件参数picWidth、picHeight决定。 
+        //图片显示大小由插件参数picWidth、picHeight决定。
+        // TODO 建议提供针对已有img对象的包装。直接获取img对象的长宽作为蒙版div的长宽。
         createHtml:function(){
             var p = this.options;
             var g = this;
+            
+            //
+            //如果目标jquerydiv中包含img元素则直接使用该img作为图片源。
+            var $img=this.getObj().children("img").first();
+            var img=$img.get(0);//dom
+            if(img){
+            	p.picUrl=$img.attr("src");
+            	p.picWidth=$img.width(); //当前展示的大小
+            	p.picHeight=$img.height(); //当前展示的大小
+            }
+            //
+            
             var htmlArr = [];
             var width = p.picWidth?'width:'+ p.picWidth+'px;':'';
             var height = p.picHeight?'height:'+ p.picHeight+'px;':'';
+
             htmlArr.push('<div style="position: relative;'+width+ height+'">');//FIXME 是不是少了结束标志？
-            htmlArr.push('<img src="'+ p.picUrl+'" style="'+width+height+'" />');
-            htmlArr.push('<div  class="modal" style="position: absolute;z-index: 99;'+width+height+';left:0;float: left;top: 0;"></div>');
+            htmlArr.push('<img class="baseDrawing" src="'+ p.picUrl+'" style="'+width+height+'" />');
+            htmlArr.push('<div class="modal" style="position: absolute;z-index: 99;'+width+height+';left:0;float: left;top: 0;"></div>');
+            htmlArr.push('</div>');
             g.getObj().html(htmlArr.join(""));
+            
+            //补齐p.picWidth p.picHeight,marks绘图使用p的这两个参数作为大小缓存参考使用。FIXME 之后应该直接根据标记板大小作为计算依据。
+            var $img= g.getObj().find("img.baseDrawing").first();
+            p.picWidth=$img.width(); //当前展示的大小
+        	p.picHeight=$img.height(); //当前展示的大小
+            
         },
 
         //创建蒙版中的标注。(wrapper)生成一个div并放入标注的img对象picObj。
@@ -93,9 +122,11 @@
             var $picture = g.getObj();
             var $modal = $picture.find(".modal");//找到蒙版
             var wrapper = $("<div style='position: absolute;' class='marker'/>");
-            wrapper.bind("click",function(){
-                g.selectedMarker = wrapper;
+            //addMarker的时候增加单击事件
+            wrapper.bind("click",function(){//单击事件
+                g.selectedMarker = wrapper; //单击选中
             }) ;
+            
             wrapper.css({left: x,top: y});
             picObj.appendTo(wrapper);
             $modal.append(wrapper);
@@ -124,12 +155,12 @@
             $picture.click(function(e){
                 if(g.getFlag()>0&& p.isEdit){ //编辑模式时才添加具体标注对象 //flag？难道是点击markid对象后的状态？
                     var target = $($markObj.find("img")[g.getFlag()-1]).clone(); //获取点击markObjdom对象（img）并复制一份。
-                    //计算位置？
-                    var left = e.clientX- $modal.parent()[0].offsetLeft;
+                    //计算需要标注的位置
+                    var left = e.clientX- $modal.parent()[0].offsetLeft;//offsetLeft? 
                     var top = e.clientY-$modal.parent()[0].offsetTop;
                     //生成标注对象并包装标注图标
                     var wrapper = g.addMarker(left,top,target);
-                    g.selectedMarker = wrapper;
+                    g.selectedMarker = wrapper; //刚刚新增的元素直接为选中元素。
                     g.setFlag( -1);
 
                     $picture.css({cursor:"pointer"}); //鼠标样式复原
@@ -172,20 +203,31 @@
                 type:"post",
                 url: p.markerUrl,
                 dataType:"json",
-                success:function(datas){
-
-                    for(var i= 0;i<datas.length;i++){
-                        var data = datas[i];
-                        var $pic = $("<img src='"+data.url+"'/>") ;
-                        var $wrap = g.addMarker(data.x,data.y,$pic);
-                        if(!p.isEdit){
+                success:function(data){
+                	
+                	var marks=data.marks;
+                	//读取数据后根据获取的数据直接构造标记
+                    for(var i= 0;i<marks.length;i++){
+                        var mark = marks[i];
+                        var $pic = $("<img src='"+mark.url+"'/>") ;
+                        
+                        //使用比例计算而不是使用点状设置
+//                        var x=mark.x
+//                        var y=mark.y
+                        var x=Math.round(mark.xp*p.picWidth);
+                        var y=Math.round(mark.yp*p.picHeight);
+                        //
+//                        alert(""+x+""+y);
+                        
+                        var $wrap = g.addMarker(x,y,$pic);
+                        if(!p.isEdit){ //当不是编辑模式时，提供一个viewClick函数回调，在非编辑模式中提供单击事件扩展。
                             $wrap.click(function(){
                                 if(p.viewClick){
-                                    p.viewClick.call(this,$wrap,data);
+                                    p.viewClick.call(this,$wrap,mark);
                                 }
-                            })
-                        }else{
-                            g.addDblEvent($wrap,data) ;
+                            });
+                        }else{ //如果是编辑模式
+                            g.addDblEvent($wrap,mark) ;
                         }
 
                     }
@@ -203,6 +245,7 @@
             var $modal = $picture.find(".modal");
 
             //标注对象的双击事件
+            //该部分做了扩展框架，主要是引用artDialog或iframe提供编辑框。并提供回调函数支持。
             wrapper.dblclick(function(){
                 params.x = parseInt(wrapper.css("left"));
                 params.y = parseInt(wrapper.css("top"));
@@ -292,11 +335,21 @@
          * @return {Array}
          */
         getMarkerInfo:function(){
+        	var reObj={};
             var ret = [];
             var g = this;
+            var p = this.options;
             var $picture = g.getObj();
             var $modal = $picture.find(".modal");
 
+            //标注板信息
+            var markBoard={};
+            markBoard.picUrl=p.picUrl;
+            markBoard.width=p.picWidth;
+            markBoard.height=p.picHeight;
+            reObj.markBoard=markBoard;
+            
+            //标注集合信息
             var $markers = $(".marker",$modal);
             var info;
             var $marker;
@@ -305,19 +358,25 @@
                 $marker = $($markers[i]);
                 info.x = parseInt($marker.css("left"));
                 info.y = parseInt($marker.css("top"));
+                //增加百分比记录 FIXME临时使用option中的参数，其实应该提供一个缓存。
+                info.xp = info.x / p.picWidth;
+                info.yp = info.y / p.picHeight;
+                //
                 info.url = $marker.find("img").attr("src");
                 ret.push(info);
             }
-            return ret;
+            //复杂结构
+            reObj.marks=ret;
+            return reObj;
 
         },
+        //删除所选标注对象
         deleteSelectedMarker:function(){
             var g = this;
             if(g.selectedMarker){
                 g.selectedMarker.remove();
                 g.selectedMarker = null;
             }
-
         }
 
 
